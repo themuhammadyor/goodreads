be5c3cca1afb530ca984eb45df9fde59fbc93c35
themuhammadyor <muhammadyor.2022@gmail.com>
2024-02-21 15:00:51 +0500

    feat: bookreview

--- apps/books/views.py
+++ apps/books/views.py
@@ -1,29 +1,59 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views import View
+from django.views.generic import DetailView, ListView
 
-from apps.books.models import Book, BookAuthor
+from apps.books.forms import AddBookReviewForm
+from apps.books.models import Book, BookReview
+from apps.users.models import User
 
 
-class BookListView(ListView):
-    model = Book
-    template_name = "books/book-list.html"
-    context_object_name = "books"
+class BookListView(View):
+    def get(self, request):
+        queryset = Book.objects.all()
+        param = request.GET.get("q", None)
 
-class BookDetailView(DetailView):
-    model = Book
-    slug_url_kwarg = 'slug'
+        if param is not None:
+            queryset = queryset.filter(title__icontains=param)
+        context = {
+            "books": queryset,
+            "param": param
+        }
+        return render(request, "books/book-list.html", context=context)
+
+
+class BookDetailView(View):
+    def get(self, request, slug):
+        book = Book.objects.get(slug=slug)
+        form = AddBookReviewForm()
+        context = {
+            "book": book,
+            "form": form
+        }
+        return render(request, "books/book-detail.html", context=context)
+
+
+class AddReviewView(View):
+    def post(self, request, pk):
+        book = Book.objects.get(id=pk)
+        user = User.objects.get(username=request.user.username)
+        form = AddBookReviewForm(request.POST)
+        if form.is_valid():
+            BookReview.objects.create(
+                user=user,
+                book=book,
+                body=form.cleaned_data.get("body"),
+                rating=form.cleaned_data.get("rating")
+            )
+            return redirect(reverse("books:book-detail", kwargs={"slug": book.slug}))
+        else:
+            context = {
+                "book": book,
+                "form": form
+            }
+            return render(request, "books/book-detail.html", context=context)
+
+
+class BookReviewsView(ListView):
+    model = BookReview
     template_name = "books/book-detail.html"
-    context_object_name = "book"
-
-class AuthorListView(ListView):
-    model = BookAuthor
-    template_name = "books/author-list.html"
-    context_object_name = "authors"
-
-class AuthorDetailView(DetailView):
-    model = Book
-    slug_url_kwarg = 'slug'
-    template_name = "books/author-book.html"
-    context_object_name = "authors"
-
-
